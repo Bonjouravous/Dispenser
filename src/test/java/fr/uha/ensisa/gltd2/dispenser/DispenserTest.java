08e7332e3aea2a56e7b8b6175ba2c26a4768c910
Bonsoiravous <alexis.decker-wurtz@uha.fr>
2018-10-12 22:56:00 +0200

    test

--- src/test/java/fr/uha/ensisa/gltd2/dispenser/DispenserTest.java
+++ src/test/java/fr/uha/ensisa/gltd2/dispenser/DispenserTest.java
@@ -45,7 +45,7 @@ public class DispenserTest {
 		verifyZeroInteractions(dummyProvider);
 		sut.select(coca);
 		verify(dummyCashController).givebackMoney(100);
-		verify(dummyProvider).giveDrink(sprite);
+		verify(dummyProvider).giveDrink(coca);
 	}
 
 }

084fcc10b50bfd078637e180178e9536e01a7fe9
Bonsoiravous <alexis.decker-wurtz@uha.fr>
2018-10-13 21:55:27 +0200

    change test drink

--- src/test/java/fr/uha/ensisa/gltd2/dispenser/DispenserTest.java
+++ src/test/java/fr/uha/ensisa/gltd2/dispenser/DispenserTest.java
@@ -44,7 +44,7 @@ public class DispenserTest {
 		sut.pay(300);
 		verifyZeroInteractions(dummyProvider);
 		sut.select(coca);
-		verify(dummyCashController).givebackMoney(200);
+		verify(dummyCashController).givebackMoney(100);
 		verify(dummyProvider).giveDrink(coca);
 	}
 
